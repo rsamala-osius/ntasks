6b45b17bd0bac2b063bd8e9ce6e1c4cf060cfd77
Raju Samala <rsamala.osicus@gmail.com>
2017-04-04 13:51:38 +0530

    PopulateDB.js added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express";
 import consign from "consign";
+import mongoose from "mongoose";
 
 const PORT = 3000;
 const app = express();
+
+var mongoDB = 'mongodb://raju:raju12@ds151450.mlab.com:51450/employee';
+mongoose.connect(mongoDB);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//  mongodb://<dbuser>:<dbpassword>@ds151450.mlab.com:51450/employee   -- raju/raju12
 
 app.set('json spaces',4);
 
